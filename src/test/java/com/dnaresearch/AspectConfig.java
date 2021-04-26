be93668eea370c589437dcdbe88acd0aaa9b8e00
Audinéia Carmo <audineia.carmo@ilia.digital>
2021-04-26 17:01:03 -0300

    :recycle: Implements the Aspect

---
+++ src/test/java/com/dnaresearch/AspectConfig.java
@@ -0,0 +1,10 @@
+package com.dnaresearch;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AspectConfig {
+
+}
