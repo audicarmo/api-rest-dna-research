7348c43c14f754305f8f913288c3cc472924bd4f
Audinéia Carmo <audineia.carmo@ilia.digital>
2021-04-26 16:57:34 -0300

    :recycle: Introduction to the aesthetic pattern with Lint

--- src/main/java/com/dnaresearch/DnaResearchApplication.java
+++ src/main/java/com/dnaresearch/DnaResearchApplication.java
@@ -1,13 +1,23 @@
 package com.dnaresearch;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DnaResearchApplication {
 
+	@SuppressWarnings("checkstyle:finalparameters")
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(DnaResearchApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
--- src/test/java/com/dnaresearch/DnaResearchApplicationTests.java
+++ src/test/java/com/dnaresearch/DnaResearchApplicationTests.java
@@ -1,9 +1,20 @@
 package com.dnaresearch;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DnaResearchApplicationTests {
 
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void initApplication() {
+        DnaResearchApplication.main(new String[] {});
+    }
 }
