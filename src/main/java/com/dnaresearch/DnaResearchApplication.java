cb118aadf6fbde029980ab0f25051420ff360481
Audinéia Carmo <audineia.carmo@ilia.digital>
2021-04-26 10:49:25 -0300

    :sparkles: Project start

---
+++ src/main/java/com/dnaresearch/DnaResearchApplication.java
@@ -0,0 +1,13 @@
+package com.dnaresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaResearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnaresearch/DnaResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnaresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
